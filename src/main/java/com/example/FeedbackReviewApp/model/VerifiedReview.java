060468a0e9326e02882a56854275d91dfd674e29
It24103762 <it24103762@gmail.com>
2025-05-21 16:50:20 +0530

    verified review class created and inherit with review

--- src/main/java/com/example/FeedbackReviewApp/model/VerifiedReview.java
+++ src/main/java/com/example/FeedbackReviewApp/model/VerifiedReview.java
@@ -1,4 +1,14 @@
 package com.example.FeedbackReviewApp.model;
 
-public class VerifiedReview {
+public class VerifiedReview extends Review{
+
+    public VerifiedReview(String reviewId, String vendorName, String reviewerName, String feedback, int rating) {
+        super(reviewId, vendorName, reviewerName, feedback, rating, true);
+    }
+
+    @Override
+    public String displayReview() {
+        return "Verified Review for " + getVendorName() + " by " + getReviewerName() + ": " + getFeedback() + " (Rating: " + getRating() + ") [Verified]";
+    }
+    
 }
