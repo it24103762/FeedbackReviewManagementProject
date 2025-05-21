5f69f5df3e546c8903627639f2fc914d4fe7b0f2
It24103762 <it24103762@gmail.com>
2025-05-21 21:52:07 +0530

    [200~

--- src/main/java/com/example/FeedbackReviewApp/controller/ReviewController.java
+++ src/main/java/com/example/FeedbackReviewApp/controller/ReviewController.java
@@ -6,6 +6,7 @@ import com.example.FeedbackReviewApp.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.example.FeedbackReviewApp.dto.ReviewRequest;
 
 
 import java.io.IOException;
--- src/main/java/com/example/FeedbackReviewApp/dto/ReviewRequest.java
+++ src/main/java/com/example/FeedbackReviewApp/dto/ReviewRequest.java
@@ -1,4 +1,50 @@
 package com.example.FeedbackReviewApp.dto;
 
 public class ReviewRequest {
+    private String vendorName;
+    private String reviewerName;
+    private String feedback;
+    private int rating;
+    private boolean isVerified;
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getReviewerName() {
+        return reviewerName;
+    }
+
+    public void setReviewerName(String reviewerName) {
+        this.reviewerName = reviewerName;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public boolean isVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(boolean verified) {
+        isVerified = verified;
+    }
+
 }
