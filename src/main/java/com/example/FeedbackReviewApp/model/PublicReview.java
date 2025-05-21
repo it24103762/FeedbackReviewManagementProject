5cc3874d54238cd06197cdb17ae2eeaca91335bf
It24103762 <it24103762@gmail.com>
2025-05-21 16:32:32 +0530

    public review class created and inherite with review

--- src/main/java/com/example/FeedbackReviewApp/model/PublicReview.java
+++ src/main/java/com/example/FeedbackReviewApp/model/PublicReview.java
@@ -1,4 +1,17 @@
 package com.example.FeedbackReviewApp.model;
 
-public class PublicReview {
+public class PublicReview extends Review{
+
+
+
+    public PublicReview(String reviewId, String vendorName, String reviewerName, String feedback, int rating) {
+        super(reviewId, vendorName, reviewerName, feedback, rating, false);
+    }
+
+    @Override
+    public String displayReview() {
+        return "Public Review for " + getVendorName() + " by " + getReviewerName() + ": " + getFeedback() + " (Rating: " + getRating() + ")";
+    }
+
+
 }
