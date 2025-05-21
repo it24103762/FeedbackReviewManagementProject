471745f0bd6817b766cd80a09588f83785090083
It24103762 <it24103762@gmail.com>
2025-05-21 16:06:09 +0530

    Abstract review class is created with constrectures and getters setters

--- src/main/java/com/example/FeedbackReviewApp/model/Review.java
+++ src/main/java/com/example/FeedbackReviewApp/model/Review.java
@@ -1,4 +1,94 @@
 package com.example.FeedbackReviewApp.model;
 
-public class Review {
+public abstract class Review {
+    private String reviewId;
+    private String vendorName;
+    private String reviewerName;
+    private String feedback;
+    private int rating;
+    private boolean isVerified;
+
+
+    public Review(String reviewId, String vendorName, String reviewerName, String feedback, int rating, boolean isVerified) {
+        this.reviewId = reviewId;
+        this.vendorName = vendorName;
+        this.reviewerName = reviewerName;
+        this.feedback = feedback;
+        this.rating = rating;
+        this.isVerified = isVerified;
+    }
+
+
+    public String getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(String reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getReviewerName() {
+        return reviewerName;
+    }
+
+    public void setReviewerName(String reviewerName) {
+        this.reviewerName = reviewerName;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public boolean isVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(boolean verified) {
+        isVerified = verified;
+    }
+
+
+    public abstract String displayReview();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
