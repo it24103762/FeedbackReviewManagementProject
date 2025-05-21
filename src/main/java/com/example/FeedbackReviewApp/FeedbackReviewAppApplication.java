7966e443b644140eb6c4323ac98f0f994606146e
It24103762 <it24103762@gmail.com>
2025-05-21 13:05:14 +0530

    init project

---
+++ src/main/java/com/example/FeedbackReviewApp/FeedbackReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.FeedbackReviewApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackReviewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FeedbackReviewApp/FeedbackReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FeedbackReviewApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
